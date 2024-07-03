ce724de8f0bd6757d9fc5fe41de7c2acd7a1a930
wang19891218 <wang19891218@gmail.com>
2024-07-03 09:16:03 -0700

    disable unused plugins

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -15,45 +15,45 @@ return {
 	},
 
 	-- Incremental rename
-	{
-		"smjonas/inc-rename.nvim",
-		cmd = "IncRename",
-		config = true,
-	},
+	--{
+	--	"smjonas/inc-rename.nvim",
+	--	cmd = "IncRename",
+	--	config = true,
+	--},
 
 	-- Refactoring tool
-	{
-		"ThePrimeagen/refactoring.nvim",
-		keys = {
-			{
-				"<leader>r",
-				function()
-					require("refactoring").select_refactor()
-				end,
-				mode = "v",
-				noremap = true,
-				silent = true,
-				expr = false,
-			},
-		},
-		opts = {},
-	},
+	--{
+	--	"ThePrimeagen/refactoring.nvim",
+	--	keys = {
+	--		{
+	--			"<leader>r",
+	--			function()
+	--				require("refactoring").select_refactor()
+	--			end,
+	--			mode = "v",
+	--			noremap = true,
+	--			silent = true,
+	--			expr = false,
+	--		},
+	--	},
+	--	opts = {},
+	--},
 
 	-- Go forward/backward with square brackets
-	{
-		"echasnovski/mini.bracketed",
-		event = "BufReadPost",
-		config = function()
-			local bracketed = require("mini.bracketed")
-			bracketed.setup({
-				file = { suffix = "" },
-				window = { suffix = "" },
-				quickfix = { suffix = "" },
-				yank = { suffix = "" },
-				treesitter = { suffix = "n" },
-			})
-		end,
-	},
+	-- {
+	--	"echasnovski/mini.bracketed",
+	--	event = "BufReadPost",
+	--	config = function()
+	--		local bracketed = require("mini.bracketed")
+	--		bracketed.setup({
+	--			file = { suffix = "" },
+	--			window = { suffix = "" },
+	--			quickfix = { suffix = "" },
+	--			yank = { suffix = "" },
+	--			treesitter = { suffix = "n" },
+	--		})
+	--	end,
+	--},
 
 	-- Better increase/descrease
 	{
